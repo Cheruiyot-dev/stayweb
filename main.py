4554e272485a7c71795e48443d8429f07c1565b1
Cheruiyot-dev <allankipkoech9@gmail.com>
2024-09-17 12:10:07 +0300

    configure env

--- main.py
+++ main.py
@@ -1,46 +1,48 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from app.routes.guest_routes import guest_bp
 from app.routes.admin_routes import admin_bp
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from config import db
-import os
 
 # Import your models after db is defined
 from app.models import Guest, Room, Booking, Table, TableReservation, Payment, RoomCategory
 
-# Initialize the app
+# Load environment variables from .env file
+load_dotenv()
+
+# Initialize the Flask app
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 
-app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
+# Set a secret key for sessions (loaded from environment variables)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-# Load config from a config file or environment variables
-# app.config.from_object('config.Config')
+# Load the SQLAlchemy database URL from environment variables
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:1092@localhost/havenstaydb'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://havenstaydb_user:VCTJku33iQdUah7E5uLNLCGgXonRj1aB@dpg-crkculbv2p9s73b5ehsg-a.oregon-postgres.render.com/havenstaydb'
-
-
+# Initialize the database with the app
 db.init_app(app)
+
+# Initialize Flask-Migrate for handling migrations
+migrate = Migrate(app, db)
 
 # Register Blueprints for guest and admin routes
 app.register_blueprint(guest_bp, url_prefix='/')
 app.register_blueprint(admin_bp, url_prefix='/admin')
 
-migrate = Migrate(app, db)
-
-def create_tables():
+def run_migrations():
     """
-    Function to create tables if they don't exist
+    Run migrations automatically when the app starts.
     """
     with app.app_context():
         try:
-            db.create_all()
-            print("Database tables created successfully.")
+            upgrade()  # Automatically run flask db upgrade
+            print("Database migration applied successfully.")
         except Exception as e:
-            print(f"Error creating tables: {e}")
+            print(f"Error running migrations: {e}")
 
 if __name__ == "__main__":
-    # Ensure tables are created before running the app
-    with app.app_context():
-        create_tables()
+    # Run migrations and start the Flask application
+    run_migrations()
     app.run(debug=True)
